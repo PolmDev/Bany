95222ecefa172e170a901bd623f0ea085d767824
tero <sotetero11@gmail.com>
2024-07-13 15:07:04 +0900

    voice setting

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits } from "discord.js";
+import {
+  createAudioPlayer,
+  createAudioResource,
+  getVoiceConnection,
+  joinVoiceChannel,
+} from "@discordjs/voice";
+import ytdl from "ytdl-core";
 
 const bany = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 });
 
@@ -15,6 +23,38 @@ bany.on("ready", async () => {
 
 bany.on("messageCreate", (msg) => {
   console.log(msg.content);
+
+  if (msg.content == "join") {
+    const channel = msg.member?.voice.channel;
+
+    if (channel) {
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator,
+      });
+
+      const stream = ytdl("https://www.youtube.com/watch?v=sSsI7ktL4Ok", {
+        filter: "audioonly",
+      });
+      const resource = createAudioResource(stream);
+      const player = createAudioPlayer();
+
+      player.play(resource);
+      connection.subscribe(player);
+    } else {
+      console.log("음성채널에 들어가주세요");
+    }
+  } else if (msg.content == "leave") {
+    const guildId = msg.guild?.id;
+    if (guildId) {
+      const connection = getVoiceConnection(guildId);
+
+      if (connection) {
+        connection.destroy();
+      }
+    }
+  }
 });
 
 bany.on("interactionCreate", async (interaction) => {});
